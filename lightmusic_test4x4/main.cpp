9b5f5f14e928a71c8acfc345abfb150467ea5dd0
Dylan van Eck <dylan.v.eck@hotmail.com>
2017-07-06 02:11:09 +0200

    author header

--- lightmusic_test4x4/main.cpp
+++ lightmusic_test4x4/main.cpp
@@ -1,3 +1,26 @@
+==========================================================================================
+//  ___               __      __                                            
+// /\_ \   __        /\ \    /\ \__                            __          
+// \//\ \ /\_\     __\ \ \___\ \ ,_\   ___ ___   __  __   ____/\_\    ___   
+//   \ \ \\/\ \  /'_ `\ \  _ `\ \ \/ /' __` __`\/\ \/\ \ /',__\/\ \  /'___\ 
+//    \_\ \\ \ \/\ \L\ \ \ \ \ \ \ \_/\ \/\ \/\ \ \ \_\ /\__, `\ \ \/\ \__/ 
+//    /\____\ \_\ \____ \ \_\ \_\ \__\ \_\ \_\ \_\ \____\/\____/\ \_\ \____\
+//    \/____/\/_/\/___L\ \/_/\/_/\/__/\/_/\/_/\/_/\/___/ \/___/  \/_/\/____/
+//                 /\____/                                                  
+//                 \_/__/                                                   
+//
+//           File      : main.cpp
+//           Part of   : C++ Lightmusic,
+//           Author    : Dylan van Eck
+//           Github    : https://github.com/howtoprogrambirds/IPASS
+//           Copyright : dylan.vaneck@student.hu.nl 2017
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//(See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+==========================================================================================
+
+
 #include "hwlib.hpp"
 #include "stdint.h"
 #include "midi.hpp"
